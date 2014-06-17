8615f4d8f45ce5100266cca5a40fc798222ab045
Afton <afton@howtoprocrastinate.com>
2014-06-17 08:27:34 -0700

    First attempt at caching. Untested, and not sure what to make of the <<- operator. Looks like a 'dynamic' operator, to lift things from a scope to an enclosing scope

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,9 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  
+  return(x)
 }
 
 
@@ -12,4 +14,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if ( ! is.null(x$inverse) )
+  {
+    return ( x$inverse)
+  }
+  
+  x$inverse <- solve(x)
+  
+  return(x$inverse)
+  
 }
